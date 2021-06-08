d8860c9515da8e991fa44ba3c53dcaccc3284965
ttony0321 <48801180+ttony0321@users.noreply.github.com>
2021-06-08 22:21:35 +0900

    Initial commit

---
+++ officecon/settings/dev.py
@@ -0,0 +1,8 @@
+from .base import *
+
+DEBUG = True
+ENVIRONMENT_NAME = "Dev Server"
+ENVIRONMENT_COLOR = "#FF2222"
+
+#ENVIRONMENT_ADMIN_SELECTOR = "grp-header"
+#ENVIRONMENT_FLOAT = True
---
+++ officecon/settings/prod.py
@@ -0,0 +1,6 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+ENVIRONMENT_NAME = "Prod Server"
+ENVIRONMENT_COLOR = "#2e4bdb"
